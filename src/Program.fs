b6fee42d94c32eb29e411bad175e8bd56daa337c
Ruxo Zheng <rz@just.sent.as>
2022-04-10 08:45:05 +0700

    Install Paket

--- src/Program.fs
+++ src/Program.fs
@@ -2,14 +2,15 @@ open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 
+#nowarn "20"
+
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
     let app = builder.Build()
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    app.MapGet("/", Func<string>(fun () -> "Hello World!"))
 
     app.Run()
 
     0 // Exit code
-
